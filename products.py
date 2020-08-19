f12888a3beb22f6a9b6f782a24c3b2fcf098189f
Tai <st30102z@gmail.com>
2020-08-19 14:31:55 +0800

    add if __name__ == '__main__':

--- products.py
+++ products.py
@@ -1,12 +1,44 @@
-products = []
-while True:
-	name = input('請輸入商品名稱:')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格: ')
-	products.append([name, price])
-print(products)
+import os
 
-for p in products:
-	print(p[0],'的價格是', p[1])
+# 讀取檔案
+def read_file(filename):
+	products = []
+	with open(filename,'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #繼續
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	return products
 
+# 讓使用者輸入
+def user_input(products):
+	while True:
+		name = input('請輸入商品名稱:')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格: ')
+		price = int(price)
+		products.append([name, price])
+	print(products)
+	return products
+
+# 印出所有購買紀錄
+def print_products(products):
+	for p in products:
+		print(p[0],'的價格是', p[1])
+
+# 寫入檔案
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):
+		print('yeah! 找到檔案了!')
+		products = read_file(filename)
+	else:
+		print('找不到檔案')
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+if __name__ == '__main__':
+	main()
